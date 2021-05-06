b5f5acf422d7de9e20d4ab5037d3d7d4aceb1fee
Javier Garcia <rampxxxx@gmail.com>
2021-05-06 09:34:24 +0200

    pceplib: Start creating type interfaces.
    Signed-off-by: Javier Garcia <rampxxxx@gmail.com>

--- pceplib/pceplib.go
+++ pceplib/pceplib.go
@@ -5,15 +5,36 @@ import (
 )
 
 const (
-	PCEPLIB_NAME = "pceplib"
-	PCEPLIB_VERSION = 1
+	PCEPLIB_NAME        = "pceplib"
+	PCEPLIB_TYPE_HEADER = "Header"
+	PCEPLIB_TYPE_DETAIL = "Detail"
+	PCEPLIB_VERSION     = 1
 )
+
+type I interface {
+	getType() string
+}
 
 type Header struct {
 	Version       uint8
 	Flags         uint8
 	MessageType   uint8
 	MessageLenght uint16
+}
+
+type Detail struct {
+	Version       uint8
+	Flags         uint8
+	MessageType   uint8
+	MessageLenght uint16
+}
+
+func (d Detail) getType() string {
+	return PCEPLIB_TYPE_DETAIL
+}
+
+func (d Header) getType() string {
+	return PCEPLIB_TYPE_HEADER
 }
 
 func FooPceplib() int {
@@ -32,3 +53,18 @@ func GetVersion() int {
 	return PCEPLIB_VERSION
 }
 
+func GetType(i I) string {
+	var dataType string
+	switch v := i.(type) {
+	case Header:
+		fmt.Printf("Header Type %T\n", v)
+		dataType = v.getType()
+	case Detail:
+		fmt.Printf("Detail Type %T\n", v)
+		dataType = v.getType()
+	default:
+		fmt.Printf("Default don't know Type %T\n", v)
+		dataType = v.getType()
+	}
+	return dataType
+}
--- pceplib/pceplib_test.go
+++ pceplib/pceplib_test.go
@@ -11,3 +11,14 @@ func TestVersion(t *testing.T) {
 		t.Error("Version expected ", PCEPLIB_VERSION, " got ", version)
 	}
 }
+func TestType(t *testing.T) {
+	var i I
+	var h Header
+	//var d Detail
+	i = h
+	dataType := GetType(i)
+
+	if dataType != PCEPLIB_TYPE_HEADER {
+		t.Error("Type expected ", PCEPLIB_TYPE_HEADER, " got ", dataType)
+	}
+}
